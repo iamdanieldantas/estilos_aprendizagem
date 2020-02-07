4c8e5535d85879e0d30fb4ec8bc0d53c5d5ff0f0
ddvieira <dvieira.dantas98@gmail.com>
2020-02-07 20:06:57 -0300

    Build files for Github pages

---
+++ precache-manifest.1e738f93e3fec146498eab9ae0c350db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489e5b256fa7bec8ca7d70679da9ddb5",
+    "url": "/ddvieira/estilos_aprendizagem.git/index.html"
+  },
+  {
+    "revision": "0d387a28348e1d9eec5d",
+    "url": "/ddvieira/estilos_aprendizagem.git/static/css/main.16837024.chunk.css"
+  },
+  {
+    "revision": "dd918c619c7df5daae51",
+    "url": "/ddvieira/estilos_aprendizagem.git/static/js/2.f47d94e6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ddvieira/estilos_aprendizagem.git/static/js/2.f47d94e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d387a28348e1d9eec5d",
+    "url": "/ddvieira/estilos_aprendizagem.git/static/js/main.9c05cb51.chunk.js"
+  },
+  {
+    "revision": "7b83eca458f5ef29f2d0",
+    "url": "/ddvieira/estilos_aprendizagem.git/static/js/runtime-main.5915c70c.js"
+  }
+]);
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">Questionário de Estilo de Aprendizagem</header>
+);
+
+export default Header;
