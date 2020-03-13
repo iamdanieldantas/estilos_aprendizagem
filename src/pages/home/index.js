62c530ee91de1f1ebcbae59ca1227ae0e396231a
ddvieira <dvieira.dantas98@gmail.com>
2020-03-12 21:36:53 -0300

    Finalização do formulário

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import LikertScale from 'likert-react';
+// import LikertScale from 'likert-react';
 
 export default class Home extends Component {
 
     render() {
-        const onClick = (q, n) => console.info('question: ' + q + ' answer: ' + n);
-        const reviews = [
-            { question: 'Code is readable' },
-            { question: 'Tests are comprehensive' },
-            { question: 'Webpack is configured well' }
-          ];
+        // const onClick = (q, n) => console.info('question: ' + q + ' answer: ' + n);
+        // const reviews = [
+        //     { question: 'Code is readable' },
+        //     { question: 'Tests are comprehensive' },
+        //     { question: 'Webpack is configured well' }
+        //   ];
 
         return(
-        <LikertScale
-            reviews={reviews}
-            onClick={onClick}
-            full-line
-        />)
+        // <LikertScale
+        //     reviews={reviews}
+        //     onClick={onClick}
+        //     full-line
+        // />
+        <div></div>
+        )
     }
 }
--- src/pages/quiz/index.js
+++ src/pages/quiz/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import api from '../../services/api';
+// import api from '../../services/api';
 import './styles.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import FontSizeChanger from 'react-font-size-changer'
-
-// import LikertScale from 'likert-react';
+import VolumeUp from '@material-ui/icons/VolumeUp';
+// import Likert from 'react-likert-scale';
+// import { render } from 'react-dom';
 
 export default class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answer1: 0,
+            answer2: 0,
+            answer3: 0,
+            answer4: 0,
+            answer5: 0,
+            answer6: 0,
+        };
+    }
 
     //Execute when page loads
     componentDidMount() {
@@ -17,43 +29,66 @@ export default class Main extends Component {
         let table = []
 
         // Outer loop to create parent
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < questions.length; i++) {
             let children = []
             //Inner loop to create children
-            children.push(<td>{`Column ${i + 1}`}</td>)
-            // for (let j = 0; j < 5; j++) {
-            // }
+            children.push(<td>{`${i + 1}: ${questions[i]}`}</td>)
             //Create the parent and add the children
-            table.push(<tr>{children}</tr>)
+            table.push(<tr>{children}</tr>);
+            table.push(<i className="audio-icon far fa-play-circle"></i>);
+            table.push(this.createLikertScale(i + 1));
         }
         return table
     }
 
-    loadProducts = async () => {
-        //Get all the products from the api setted before
-        //Using the library axios
-        const response = await api.get('/products');
+    createLikertScale = (idNumber) => {
+        return (
+            <div className="wrap">
+                <VolumeUp />
+                <form action="">
+                    <ul className='likert' id={`${idNumber}`}>
+                        <li>
+                            <input type="radio" name="likert" value="1" onClick={this.submiteAnswer.bind(this, idNumber, 1)} />
+                            <label>Concordo totalmente</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="likert" value="2" onClick={this.submiteAnswer.bind(this, idNumber, 2)} />
+                            <label>Concordo parcialmente</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="likert" value="3" onClick={this.submiteAnswer.bind(this, idNumber, 3)} />
+                            <label>Neutro</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="likert" value="4" onClick={this.submiteAnswer.bind(this, idNumber, 4)} />
+                            <label>Discordo parcialmente</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="likert" value="5" onClick={this.submiteAnswer.bind(this, idNumber, 5)} />
+                            <label>Discordo totalmente</label>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        )
+    }
 
-        //Set value from api to var state
-        this.setState({ products: response.data.docs });
-    };
+    submiteAnswer = (pergunta, resposta) => {
+        console.log("pergunta: " + pergunta);
+        console.log("resposta: " + resposta);
+
+        answers[pergunta-1] = resposta;
+        console.log(answers);
+        console.log();
+    }
 
     //Keep listening to var state 
     render() {
-        questions.map(tech =>
-            <p>tech</p>)
-        // const onClick = (q, n) => console.info('question: ' + q + ' answer: ' + n);
-        // const reviews = [
-        //     { question: 'Você gosta de site com textos?' },
-        //     { question: 'Você prefere conteúdo que seja em vídeo?' },
-        //     { question: 'Você prefere conteúdo com audio?' },
-        //     { question: 'Você prefere texto grande?' },
-        //     { question: 'Você prefere que os vídeos tenham legendas ?' },
-        //     { question: 'Você prefere que tenha ?' }
-        // ];
 
         return (
             <div className='questions-list' >
+                <i className="far fa-play-circle"></i>
+                <i className="far fa-play-circle"></i>
                 <div className="app">
                     <FontSizeChanger
                         targets={['#target .content']}
@@ -61,11 +96,9 @@ export default class Main extends Component {
                             console.log(element, newValue, oldValue);
                         }}
 
-                        //stepSize = font size
-                        //range = stepts to change
                         options={{
-                            stepSize: 2,
-                            range: 4
+                            stepSize: 2, //stepSize = font size
+                            range: 4 //range = stepts to change
                         }}
                         customButtons={{
                             up: <span style={{ 'fontSize': '36px' }}>A</span>,
@@ -81,29 +114,33 @@ export default class Main extends Component {
                         }}
                     />
                     <div id="target">
-                        <p className="title">This is the title of my target text</p>
-                        {/* <p className="content">This is the content of my target text</p> */}
-
+                        <p className="title">Responda o questionário abaixo:</p>
                         <table className="content">
-                            {this.createTable()}
-                            {/* likert scale */}
+                            <thead>
+
+                            </thead>
+                            <tbody>
+                                {this.createTable()}
+                            </tbody>
+
                         </table>
                     </div>
                 </div>
 
-                {/* <LikertScale
-                    reviews={reviews}
-                    onClick={onClick}
-                    class="likert"
-                /> */}
-
                 <div className="actions" >
-                    <Link to="/home">Acessar site</Link>
+                    <button onClick={this.submiteAnswer}>Submit</button>
                 </div>
-
             </div >
         );
     }
 }
 
-let questions = ["alguma coisa", 'alguma coisa 2'];
+let questions = ['Você gosta de site com textos?',
+    'Você prefere conteúdo que seja em vídeo?',
+    'Você prefere conteúdo com audio?',
+    'Você prefere texto grande?',
+    'Você prefere que os vídeos tenham legendas?',
+    'Você prefere que tenha?'];
+
+let answers = [];
+
