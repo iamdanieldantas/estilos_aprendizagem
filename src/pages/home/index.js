c97b581d7b683aa32ed82e69efdbc7989decf5cd
DANIEL DANTAS <dvieira.dantas98@gmail.com>
2020-04-14 12:59:20 -0300

    inclusão imagens

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,21 +5,24 @@ import VolumeUp from '@material-ui/icons/VolumeUp';
 import './style.css';
 
 import midia_1 from '../../assets/audios/Conhecimentos Básicos de Computação e Microinformática.mp3';
-import midia_2 from '../../assets/audios/ARQUITETURA SIMPLIFICADA DE UM COMPUTADOR.mp3';
-import midia_3 from '../../assets/audios/COMPONENTES BÁSICOS DE UM COMPUTADOR.mp3';
+import midia_2 from '../../assets/audios/COMPONENTES BÁSICOS DE UM COMPUTADOR.mp3';
+import midia_3 from '../../assets/audios/ARQUITETURA SIMPLIFICADA DE UM COMPUTADOR.mp3';
 import midia_4 from '../../assets/audios/FUNÇÕES DOS COMPONENTES BÁSICOS.mp3';
 import midia_5 from '../../assets/audios/UNIDADE CENTRAL DE PROCESSAMENTO.mp3';
 import midia_6 from '../../assets/audios/MEMÓRIA.mp3';
 import midia_7 from '../../assets/audios/RAM (RANDOM ACCESS MEMORY).mp3';
 import midia_8 from '../../assets/audios/ROM(READY ONLY MEMORY).mp3';
 
+import img1 from '../../assets/images/img1.png';
+import img2 from '../../assets/images/img2.png';
+
 export default function Home() {
     const result_quiz = localStorage.getItem('deficiencia');
-    const audio = new Audio(midia_1);
     alert("result: " + result_quiz);
 
-    const play = (midia) => {        
-       /* audio.play(midia);*/
+    const play = (midia) => {
+        const audio = new Audio(midia);
+        audio.play();
     }
 
     const gabarito = {
@@ -30,7 +33,7 @@ export default function Home() {
         },
 
         1: {
-            deficiencia: ['motora','auditiva', 'cognitiva'],
+            deficiencia: ['motora', 'auditiva', 'cognitiva'],
             exibir: 0
         },
 
@@ -66,42 +69,47 @@ export default function Home() {
         .map((gabarito) => (
             gabarito.exibir = 1
         ));
-        console.log(gabarito);
+    console.log(gabarito);
 
     return (
         <div className="conteudo-container">
             <div className="container" id="target-font-size">
 
-                <FontSizeChange />
                 <header className="content">
-                    <h1>Article Title</h1>
+                    <h1>CONHECIMENTOS BÁSICOS DE COMPUTAÇÃO E MICROINFORMÁTICA
+                        <span><VolumeUp onClick={() => play(midia_1)} className='volume' /></span>
+                    </h1>
                 </header>
 
+
                 <div className="article-conteudo content" >
+                    
+
+                    <FontSizeChange />
+
                     {gabarito[0].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/>
-                            <h2>Conhecimentos Básicos de Computação e Microinformática</h2>
+
                             <p>Este tutorial trará uma série de tópicos sobre noções básicas de informática,
-                               bem como uma série de conceitos sobre o mundo tecnológico. Nestas séries serão 
-                               abordados assuntos tais como: Computação, Microinformática, noções de redes de 
-                               computadores, componentes básicos dos computadores, funções dos componentes, 
-                               noções de sistema operacional Windows, serviços de Internet, instalação física 
-                               dos computadores, mais alguns temas que são abordados quase sempre na maioria das 
-                               provas em concursos que são realizados em todo o Brasil. Você aprenderá desde saber 
+                            bem como uma série de conceitos sobre o mundo tecnológico. Nestas séries serão
+                            abordados assuntos tais como: Computação, Microinformática, noções de redes de
+                            computadores, componentes básicos dos computadores, funções dos componentes,
+                            noções de sistema operacional Windows, serviços de Internet, instalação física
+                            dos computadores, mais alguns temas que são abordados quase sempre na maioria das
+                            provas em concursos que são realizados em todo o Brasil. Você aprenderá desde saber
                                o que é um computador até meios de transmissão de dados e redes de computadores.</p>
                             <br></br>
 
-                            <p> <strong>HARDWARE</strong> – Que consiste na parte física do computador, é a parte palpável, aquela 
+                            <p> <strong>HARDWARE</strong> – Que consiste na parte física do computador, é a parte palpável, aquela
                                 a qual podemos tocar e ver, incluindo os periféricos de entrada e saída como mouse
-                                 e teclado, ou seja, o hardware é tudo o que pode ser visto e tocado e, como toda 
-                                 máquina, não possui inteligência e não funciona sozinha, necessita de um comando 
-                                 de lógica para entrar em operação, é necessária assim a intervenção humana para 
+                                 e teclado, ou seja, o hardware é tudo o que pode ser visto e tocado e, como toda
+                                 máquina, não possui inteligência e não funciona sozinha, necessita de um comando
+                                 de lógica para entrar em operação, é necessária assim a intervenção humana para
                                  fazê-lo funcionar</p>
                             <br></br>
 
                             <p>
-                            <strong>SOFTWARE</strong> – São sistemas que rodam no computador. Comporta os programas 
+                                <strong>SOFTWARE</strong> – São sistemas que rodam no computador. Comporta os programas
                             que irão funcionar e informar ao hardware o que executar, de que forma e quando executa-las.
                             </p>
                             <br></br>
@@ -113,19 +121,18 @@ export default function Home() {
 
                         </article> : <></>}
 
-                        {gabarito[1].exibir ?
+                    {gabarito[1].exibir ?
                         <article>
-
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
-
-                            <h2>COMPONENTES BÁSICOS DE UM COMPUTADOR</h2>
+                            <h2>COMPONENTES BÁSICOS DE UM COMPUTADOR
+                                <span><VolumeUp onClick={() => play(midia_2)} className='volume' /></span>
+                            </h2>
                             <p>Aprendemos que o hardware é cada elemento físico que constitui o equipamento. Genericamente falando, o computador é formado por um conjunto de equipamentos que são caracterizados de acordo com sua função no sistema de arquitetura de um computador. Por tanto, existem vários tipos de componentes que formam juntos o universo do hardware de um computador.</p>
                             <br></br>
 
                             <p> A figura a seguir demonstra um exemplo simples de um computador e seus equipamentos:</p>
                             <br></br>
 
-                            <img src={img1}></img>
+                            <img src={img1} alt="descrição"></img>
                             <br></br>
 
                             <p>1 – Gabinete – contendo:</p>
@@ -163,34 +170,33 @@ export default function Home() {
                             <br></br>
                         </article> : <></>}
 
-                        {gabarito[2].exibir ?
+                    {gabarito[2].exibir ?
                         <article>
+                            <h2>ARQUITETURA SIMPLIFICADA DE UM COMPUTADOR
+                                <span><VolumeUp onClick={() => play(midia_3)} className='volume' /></span>
+                            </h2>
 
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
-
-                            <h2>ARQUITETURA SIMPLIFICADA DE UM COMPUTADOR</h2>
-                          
-                            <img src={img2}></img>
+                            <img src={img2} alt="descrição"></img>
                             <br></br>
                         </article> : <></>}
 
-                        {gabarito[3].exibir ?
+                    {gabarito[3].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
 
-                            <h2>FUNÇÕES DOS COMPONENTES BÁSICOS</h2>
-                            <br></br>
+                            <h2>FUNÇÕES DOS COMPONENTES BÁSICOS
+                                <span><VolumeUp onClick={() => play(midia_4)} className='volume' /></span>
+                            </h2>
 
                             <p>Para um tratamento adequado dos dados e para a execução de suas tarefas de forma organizada e segura, o computador precisa de uma arquitetura que, além de estabelecer seus componentes básicos, determina os seus objetivos e o funcionamento do conjunto de suas partes.</p>
                             <br></br>
                         </article> : <></>}
 
-                        {gabarito[4].exibir ?
+                    {gabarito[4].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
 
-                            <h2>UNIDADE CENTRAL DE PROCESSAMENTO – CPU</h2>
-                            <br></br>
+                            <h2>UNIDADE CENTRAL DE PROCESSAMENTO – CPU
+                                <span><VolumeUp onClick={() => play(midia_5)} className='volume' /></span>
+                            </h2>
 
                             <p>É um circuito integrado.</p>
                             <br></br>
@@ -223,47 +229,48 @@ export default function Home() {
 
                         </article> : <></>}
 
-                        {gabarito[5].exibir ?
+                    {gabarito[5].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
 
-                            <h2>MEMÓRIA</h2>
-                          
-                           <p>A memória de um computador pode ser classificada segundo esta hierarquia:</p>
+                            <h2>MEMÓRIA
+                                <span><VolumeUp onClick={() => play(midia_6)} className='volume' /></span>
+                            </h2>
+
+                            <p>A memória de um computador pode ser classificada segundo esta hierarquia:</p>
                             <br></br>
 
-                           <li>Memória Principal: nela o processador central do computador busca as instruções necessárias para executar e armazena os dados do processamento.</li>
-                           <li>Memória Secundária: usada para segmentos inativos de programas e arquivos de dados que são trazidos à memória principal quando necessário.</li>
-                           <br></br>
+                            <li>Memória Principal: nela o processador central do computador busca as instruções necessárias para executar e armazena os dados do processamento.</li>
+                            <li>Memória Secundária: usada para segmentos inativos de programas e arquivos de dados que são trazidos à memória principal quando necessário.</li>
+                            <br></br>
 
-                           <p>O processador central só executa as instruções e processa os dados que esteja na memória principal. A memória secundária é usada para organização de arquivos de dados históricos ou dados não necessários no momento para processamento.</p>
-                           <br></br>
+                            <p>O processador central só executa as instruções e processa os dados que esteja na memória principal. A memória secundária é usada para organização de arquivos de dados históricos ou dados não necessários no momento para processamento.</p>
+                            <br></br>
 
-                           <p>Memória Principal – Há duas subdivisões básicas ou tipos de memória principal nos computadores atuais – memória de acesso randômico e memória read-only(somente para leitura). Temos também a memória flash, que não será abordada neste tema, pois as duas categorias abaixo são as mais antigas e mais comumente usadas.</p>
-                           <br></br>
+                            <p>Memória Principal – Há duas subdivisões básicas ou tipos de memória principal nos computadores atuais – memória de acesso randômico e memória read-only(somente para leitura). Temos também a memória flash, que não será abordada neste tema, pois as duas categorias abaixo são as mais antigas e mais comumente usadas.</p>
+                            <br></br>
 
                         </article> : <></>}
 
-                        {gabarito[6].exibir ?
+                    {gabarito[6].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
 
-                            <h2>RAM (RANDOM ACCESS MEMORY)</h2>
-                            <br></br>
+                            <h2>RAM (RANDOM ACCESS MEMORY)
+                            <span><VolumeUp onClick={() => play(midia_7)} className='volume' /></span>
+                            </h2>
 
-                           <p>A Memória de Acesso Randômico (RAM), do inglês Randomic Access Memory é usada para armazenamento temporário de dados ou instruções. Quando entramos com um programa em linguagem Basic em um computador pessoal, as instruções são armazenadas na RAM do computador, assim como os dados de entrada. A RAM também é conhecida como memória read-and-write, pois podemos escrever ou ler informações neste tipo de memória.</p>
+                            <p>A Memória de Acesso Randômico (RAM), do inglês Randomic Access Memory é usada para armazenamento temporário de dados ou instruções. Quando entramos com um programa em linguagem Basic em um computador pessoal, as instruções são armazenadas na RAM do computador, assim como os dados de entrada. A RAM também é conhecida como memória read-and-write, pois podemos escrever ou ler informações neste tipo de memória.</p>
                             <br></br>
 
                             <p>A capacidade da RAM do equipamento é vital, pois determina o número de instruções e a quantidade dos dados armazenados a cada vez para um processamento.</p>
-                           <br></br>
+                            <br></br>
 
-                           <p>A memória RAM é volátil – ao desligarmos o equipamento perdem-se as informações.</p>
-                           <br></br>
+                            <p>A memória RAM é volátil – ao desligarmos o equipamento perdem-se as informações.</p>
+                            <br></br>
 
-                           <p>Algumas variações da memória RAM são:</p>
-                           <br></br>
+                            <p>Algumas variações da memória RAM são:</p>
+                            <br></br>
 
-                           <p><strong>SRAM(RAM ESTÁTICA)</strong> – As informações armazenadas, neste tipo de RAM, são mantidas com uma energia de alimentação (a fonte de energia elétrica deve ser mantida mesmo com o computador desligado).</p>
+                            <p><strong>SRAM(RAM ESTÁTICA)</strong> – As informações armazenadas, neste tipo de RAM, são mantidas com uma energia de alimentação (a fonte de energia elétrica deve ser mantida mesmo com o computador desligado).</p>
                             <br></br>
 
                             <p><strong>DRAM(RAM DINÂMICA)</strong> – Os dados nela carregados precisam de um reforço elétrico para não serem perdidos. É mais lenta que a memória SRAM.</p>
@@ -271,26 +278,26 @@ export default function Home() {
 
                         </article> : <></>}
 
-                        {gabarito[7].exibir ?
+                    {gabarito[7].exibir ?
                         <article>
-                            <VolumeUp onClick={() => play(midia_1)} className='volume'/><VolumeUp onClick={() => play(midia_1)} className='volume'/>
 
-                            <h2>ROM(READY ONLY MEMORY)</h2>
-                            <br></br>
+                            <h2>ROM(READY ONLY MEMORY)
+                                <span><VolumeUp onClick={() => play(midia_8)} className='volume' /></span>
+                            </h2>
 
-                           <p>A memória Read-Only(ROM), deriva do inglês Read Only Memory, é usada para armazenar instruções e/ou dados permanentes ou raramente alterados. A informação geralmente é colocada no chip de armazenamento quando ele é fabricado e o conteúdo da ROM não pode ser alterado por um programa do usuário.</p>
+                            <p>A memória Read-Only(ROM), deriva do inglês Read Only Memory, é usada para armazenar instruções e/ou dados permanentes ou raramente alterados. A informação geralmente é colocada no chip de armazenamento quando ele é fabricado e o conteúdo da ROM não pode ser alterado por um programa do usuário.</p>
                             <br></br>
 
                             <p>As ROMs se constituem em um hardware que possui um software determinado e que não pode ser deletado pelo usuário.</p>
-                           <br></br>
+                            <br></br>
 
-                           <p>As informações armazenadas na ROM não são voláteis, isto é, não são perdidas quando há falta de energia no computador.</p>
-                           <br></br>
+                            <p>As informações armazenadas na ROM não são voláteis, isto é, não são perdidas quando há falta de energia no computador.</p>
+                            <br></br>
 
-                           <p>Existem algumas variações e evoluções deste tipo de memória:</p>
-                           <br></br>
+                            <p>Existem algumas variações e evoluções deste tipo de memória:</p>
+                            <br></br>
 
-                           <p><strong>PROM:</strong>É a ROM que pode ser programada uma única vez.</p>
+                            <p><strong>PROM:</strong>É a ROM que pode ser programada uma única vez.</p>
                             <br></br>
 
                             <p><strong>EPROM:</strong>É a ROM que pode ser apagada com o uso da luz ultravioleta(podendo ser reprogramada)</p>
@@ -303,9 +310,6 @@ export default function Home() {
                             <br></br>
 
                         </article> : <></>}
-
-                        
-
                 </div>
             </div>
         </div>
